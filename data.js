17432c5cf6f1e3ebea4039ada08f7d4e6860a937
leonidas <49210924+leonidas1312@users.noreply.github.com>
2024-10-01 22:49:03 +0300

    Add files via upload

--- data.js
+++ data.js
@@ -1,152 +1,308 @@
 export const bio = [
-  "Hello! I'm Ioannis D. Leonidas, a passionate Quantum Software Engineer focused on solving complex problems using cutting-edge quantum algorithms.",
-  "I am enthusiastic about continuous learning and improvement, especially in challenging and nearly impossible tasks.",
+  "Hello! I'm Vinay Somawat, an experienced Frontend Software Engineer with a proven ability to upgrade applications, optimize performance, and deliver innovative solutions.",
+  "I am passionate about using my technical skills to drive impactful contributions in challenging projects.",
   "Thanks for visiting!",
 ];
 
 export const skills = [
   {
-    title: "Programming Languages",
-    skillName: "Python, C, SQL, Java, Matlab ",
+    title: "Languages",
+    skillName: "JavaScript, TypeScript",
     color: "1",
-    percentage: "100",
+    percentage: "80",
   },
   {
-    title: "Quantum Computing",
-    skillName: "Quantum approximate optimization, Qubit efficient quantum algorithms",
+    title: "Frameworks/Libraries",
+    skillName: "Angular, RxJS, NGRX/NGXS",
     color: "6",
-    percentage: "90",
+    percentage: "70",
   },
   {
-    title: "Software Engineering",
-    skillName: "Data applications, Streamlit, System design",
+    title: "Design",
+    skillName: "HTML, Bootstrap, Tailwind",
     color: "4",
-    percentage: "95",
+    percentage: "70",
   },
   {
-    title: "Research",
-    skillName: "Mathematical optimization, Quantum optimization solvers",
-    color: "3",
-    percentage: "90",
+    title: "Version Control",
+    skillName: "GitHub, JIRA, Bitbucket",
+    color: "7",
+    percentage: "70",
   },
+  {
+    title: "Tools",
+    skillName: "Debugging, Chrome DevTools",
+    color: "3",
+    percentage: "80",
+  },
+//   {
+//     title: "Analytics",
+//     skillName: "CleverTap, Countly",
+//     color: "5",
+//     percentage: "50",
+//   },
+//   {
+//     title: "IDE",
+//     skillName: "VS Code",
+//     color: "6",
+//     percentage: "70",
+//   },
 ];
 
 export const projects = {
   webProjects: [
     {
-      projectName: "AngelQ - Quantum Software",
-      image: "images/angelq.png",
+      projectName: "Programming Diaries",
+      image: "images/programmingdiaries.png",
       summary:
-        "Quantum software engineer working on variational quantum algorithms for the NISQ era.",
-      preview: "https://angelquantum.com/about/",
-      techStack: ["Quantum Algorithms", "Qiskit", "Python"],
+        "Developed a full stack blog application to provide content on techical topics across the internet with admin interface.",
+      preview: "https://programmingdiaries.herokuapp.com/",
+      techStack: ["Django", "SQLite", "Bootstrap", "JavaScript", "Heroku"],
     },
     {
-      projectName: "Quantum Machine Learning Course",
-      image: "images/quantumml.png",
+      projectName: "Find Your Bank",
+      image: "images/findyourbank.png",
       summary:
-        "Co-presenter at Erasmus+ BIP Liberec Summer School, introducing quantum machine learning to participants.",
-      preview: "https://example.com/",
-      techStack: ["Quantum Computing", "Qiskit", "Python"],
+        "Developed a React application to render a list of banks fetched from API. Filtered the banks based on queries from localstorage, marked favorites banks.",
+      preview: "https://clever-fermi-0d5d76.netlify.app",
+      techStack: ["Reactjs", "Bootstrap", "JavaScript", "Netlify"],
+    },
+    {
+      projectName: "Web Portfolio",
+      image: "images/portfolio.png",
+      summary:
+        "Web Portfolio to showcase acadmics, skills, projects and contact details in better manner.",
+      preview: "https://github.com/vinaysomawat/Travographer-Portal",
+      techStack: ["HTML", "Bootstrap", "JavaScript"],
+    },
+    {
+      projectName: "Resume Builder",
+      image: "images/resume-builder.png",
+      summary:
+        "Browser based editor to build and download Resumes in a customizable templates.",
+      preview: "https://vinaysomawat.github.io/Resume-Builder",
+      techStack: ["HTML", "Bootstrap", "JavaScript"],
     },
   ],
   softwareProjects: [
     {
-      projectName: "Quantum Approximate Optimization Algorithms",
-      image: "images/qaoa.png",
+      projectName: "Pizza Ordering ChatBot",
+      image: "images/pizzaorderchatbot.png",
       summary:
-        "Studied and implemented QAOA to solve the Tail Assignment Problem in collaboration with Angelakis Research Group.",
-      preview: "https://example.com/",
-      techStack: ["QAOA", "Python", "Quantum Algorithms"],
+        "ChatBot using Dialogflow, Firebase database which stores the chat data in the realtime database.",
+      preview: "https://github.com/vinaysomawat/Pizza-Ordering-ChatBot",
+      techStack: ["Dailogflow", "Firebase"],
+    },
+    {
+      projectName: "WhatsApp-Bot",
+      image: "images/whatsappbot.jpg",
+      summary:
+        "Python script which helps to send messages to WhatsApp contacts automatically using selenium and web automation.",
+      preview: "https://github.com/vinaysomawat/WhatsApp-Bot",
+      techStack: ["Selenium", "Chrome Webdriver", "Python"],
+    },
+    {
+      projectName: "Bill Generator",
+      image: "images/billgenerator.png",
+      summary:
+        "GUI to transfer data to excel sheets and generate bills on the local shops.",
+      preview: "https://github.com/vinaysomawat/Bill-Generator",
+      techStack: ["Tkinter", "Openxlpy", "Python"],
     },
   ],
   androidProjects: [
-    
+    {
+      projectName: "NITW-CSE",
+      image: "images/nitwcse.jpg",
+      summary:
+        "The Application display details of Department courses, reference books, research, publication and faculty profile.",
+      preview: "https://github.com/vinaysomawat/NITW-CSE",
+      techStack: ["JAVA", "XML", "Android"],
+    },
+    {
+      projectName: "CareerHigh-App",
+      image: "images/carrerhigh.png",
+      summary:
+        "The Application display the webpages of website careerhigh.in in android devices.",
+      preview: "https://github.com/vinaysomawat/CareerHigh-Android",
+      techStack: ["JAVA", "XML", "Android"],
+    },
   ],
   freelanceProjects: [
-    
+    {
+      projectName: "SnylloAir.com",
+      image: "images/snylloair.png",
+      summary:
+        "Developed a company website to showcase the purpose, services and products provided by the company to audience.",
+      preview: "https://www.snylloair.com/",
+      techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
+    },
+    {
+      projectName: "Delivery+",
+      image: "images/AM-Logo-.png",
+      summary: "Android Application to display website in android devices.",
+      preview:
+        "https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
+      techStack: ["Android", "JAVA", "Play Store"],
+    },
   ],
 };
 
 export const experience = [
-  
-    title: "Angelakis Research Group",
-    duration: "Oct 2020 - Present",
-    subtitle: "Quantum Researcher",
-    details: [
-      "Worked on Quantum Approximate Optimization Algorithms (QAOA) and their applications.",
-      "Studied performance of QAOA in solving optimization problems such as the Tail Assignment Problem."
-    ],
-    tags: ["QAOA", "Quantum Algorithms", "Research"],
-    icon: "flask",
+  {
+    title: "Shiprocket (Bigfoot Solution Private Limited)",
+    duration: "September 2022 - Present",
+    subtitle: "Software Engineer",
+    details: ["Working in support and escalation team."],
+    tags: ["JavaScript", "Angular", "Bootstrap", "Nodejs", "Jenkins"],
+    icon: "truck ",
   },
   {
-    title: "AngelQ",
-    duration: "Oct 2021 - Present",
-    subtitle: "Quantum Software Engineer",
+    title: "Biofourmis India Private Limited",
+    duration: "April 2022 - Jul 2023",
+    subtitle: "Software Engineer",
     details: [
-      "Focused on developing variational quantum algorithms for the NISQ era.",
+      `Implemented microfrontends using the Module Federation Plugin in Angular.
+            Rendered components from remote modules/repositories, enabling a modular and scalable architecture`,
+      `Upgraded application with a dynamic base URL for flexible operation across multiple regions and verticals, leading to
+            recognition with the Biofourmis Bravo award in Q1, 2023.`,
+      `Implemented RxState for managing local state in components, resulting in elegant and reactive facades.`,
+      `Utilized Twilio-Video for group video call rooms and Countly for Web Analytics.`,
+      `Integrated NGXS WebSocket, enhancing state management and replacing legacy service injections.`,
+      `Implemented extensive RxJS usage for reactive programming, resulting in efficient data handling, improved state
+            management, and enhanced application performance.`,
+      `Dramatically optimized load time and performance through code splitting, lazy loading, caching, and preload`,
+      `Enhanced video call experience by implementing real-time switching of I/O devices and audio level indicators,
+            resulting in reduced audio issues and increased user satisfaction.`,
+      `Owned and led successful Angular application upgrades from version 12 to 13 and 14, improving performance
+            and enhancing features.`,
     ],
-    tags: ["Quantum Algorithms", "Qiskit", "Python"],
-    icon: "code",
+    tags: [
+      "JavaScript",
+      "Angular",
+      "RxJS",
+      "NGXS",
+      "TypeScript",
+      "RxState",
+      "Webpack",
+      "Optimization",
+    ],
+    icon: "heartbeat",
   },
   {
-    title: "Center for Quantum Technologies, Singapore",
-    duration: "Jul 2022 - Aug 2022",
-    subtitle: "Visiting Researcher",
-    details: ["Collaborated with quantum researchers on various projects."],
-    tags: ["Quantum Computing", "Research"],
-    icon: "laptop",
+    title: "Novopay Solutions Private Limited",
+    duration: "June 2020 - April 2022",
+    subtitle: "Software Engineer",
+    details: [
+      `Implemented AePS Services, including Bio-metric eKYC, Cash Withdrawal, Balance Enquiry, Mini-Statements,
+            and Money transfer.`,
+      `Implemented QR Code integration for retailer wallet money loading, Clevertap events for user action tracking,
+            Freshdesk ticketing, Chatbot services, and Retailer onboarding/admin interfaces.`,
+      `Developed retailer QR code system for seamless money loading into wallets, resulting in improved user transactions.`,
+      `Achieved 70 percent code coverage of the entire application by writing unit tests for legacy code.`,
+    ],
+    tags: ["JavaScript", "Angular", "Bootstrap", "Nodejs", "Jenkins"],
+    icon: "qrcode",
   },
   {
-    title: "Erasmus+ BIP Liberec Summer School",
-    duration: "May 2022",
-    subtitle: "Co-presenter, Quantum Machine Learning Course",
+    title: "ThinkPedia LLP",
+    duration: "May 2019 - June 2019",
+    subtitle: "SDE Intern",
     details: [
-      "Presented implementation of a quantum classifier and supervised students on quantum teleportation algorithms."
+      `Developed a customer web application for social media management, supporting the advertisement domain.`,
     ],
-    tags: ["Quantum Computing", "Teaching"],
-    icon: "chalkboard-teacher",
+    tags: ["JavaScript", "Angular", "Bootstrap", "Java", "Spring Boot"],
+    icon: "group",
   },
 ];
 
 export const education = [
   {
-    title: "MSc in Electrical and Computer Engineering",
-    duration: "2021 - Present",
-    subtitle: "Technical University of Crete, Greece",
+    title: "B.Tech. in Computer Science and Engineering",
+    duration: "2016 - 2020",
+    subtitle: "National Institute of Technology, Warangal",
     details: [],
-    tags: ["Quantum Computing", "Electrical Engineering"],
+    tags: [
+      "Data Structures & Algorithms",
+      "Operating Systems",
+      "Database Management System",
+      "Computer Networks",
+      "Compiler Designing",
+      "Cloud Computing",
+    ],
     icon: "graduation-cap",
   },
   {
-    title: "MEng in Electrical and Computer Engineering",
-    duration: "2021",
-    subtitle: "Technical University of Crete, Greece",
+    title: "Class 12th in Science and Mathematics",
+    duration: "",
+    subtitle: "Board of Secondary Education, Rajasthan",
     details: [],
-    tags: ["Electrical Engineering", "Computer Science"],
-    icon: "graduation-cap",
+    tags: ["Physics", "Chemistry", "Mathematics"],
+    icon: "book",
   },
 ];
 
 export const footer = [
- {
+//   {
+//     label: "Dev Profiles",
+//     data: [
+//       {
+//         text: "Stackoverflow",
+//         link: "https://stackoverflow.com/users/8461233/vinay-somawat",
+//       },
+//       {
+//         text: "GitHub",
+//         link: "https://github.com/vinaysomawat",
+//       },
+//       {
+//         text: "LeetCode",
+//         link: "https://leetcode.com/somawatvinay/",
+//       },
+//     ],
+//   },
+//   {
+//     label: "Resources",
+//     data: [
+//       {
+//         text: "Enable Dark/Light Mode",
+//         func: "enableDarkMode()",
+//       },
+//       {
+//         text: "Print this page",
+//         func: "window.print()",
+//       },
+//       {
+//         text: "Clone this page",
+//         link: "https://github.com/vinaysomawat/vinaysomawat.github.io",
+//       },
+//     ],
+//   },
+  {
     label: "Links",
     data: [
+    //   {
+    //     text: "Linkedin",
+    //     link: "https://www.linkedin.com/in/vinaysomawat/",
+    //   },
+    //   {
+    //     text: "Twitter",
+    //     link: "https://twitter.com/thesigmakid",
+    //   },
       {
-        text: "Thesis",
-        link: "https://dias.library.tuc.gr/view/90563?locale=en",
+        text: "Buy me a coffee",
+        link: "https://www.buymeacoffee.com/r194dME8y",
       },
       {
-        text: "AngelQ",
-        link: "https://angelquantum.com/about/",
+        text: "Github/vinaysomawat.github.io",
+        link: "https://github.com/vinaysomawat/vinaysomawat.github.io",
       },
     ],
   },
   {
     label: "copyright-text",
     data: [
-      "Ioannis D. Leonidas.",
+      "Made with &hearts; by Vinay Somawat.",
+      "&copy; No Copyrights. Feel free to use this template.",
     ],
   },
 ];
