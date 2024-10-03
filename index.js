04ebe5d0514e7d86e37beee5c7b5020b542d4796
leonidas <49210924+leonidas1312@users.noreply.github.com>
2024-10-03 18:21:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import {
 
 import { URLs } from './user-data/urls.js';
   
-  const { webProjects, softwareProjects, androidProjects, freelanceProjects } =
+  const { webProjects, softwareProjects, freelanceProjects } =
     projects;
   const { medium, gitConnected } = URLs;
   
@@ -478,7 +478,7 @@ import { URLs } from './user-data/urls.js';
   
   populateProjects(webProjects, "web-projects");
   populateProjects(softwareProjects, "software-projects");
-  populateProjects(androidProjects, "android-projects");
+  //populateProjects(androidProjects, "android-projects");
   populateProjects(freelanceProjects, "freelance-projects");
   
   populateExp_Edu(experience, "experience");
