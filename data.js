b8fc5a38150cf2b87290145cc2f5c35bf0f37492
leonidas <49210924+leonidas1312@users.noreply.github.com>
2024-10-03 18:11:47 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,149 +1,67 @@
 export const bio = [
-  "Hello! I'm Vinay Somawat, an experienced Frontend Software Engineer with a proven ability to upgrade applications, optimize performance, and deliver innovative solutions.",
-  "I am passionate about using my technical skills to drive impactful contributions in challenging projects.",
-  "Thanks for visiting",
+  "Passionate about continuous improvement and intrigued by nearly impossible tasks. In my free time I like to",
+  "play football and chess. If I was to be asked, what characteristics describe my professional approach, the following would be the most accurate:",
+"• Problem solver",
+"• Team player, Communicator",
+"• Resilient, Coherent",
 ];
 
 export const skills = [
   {
-    title: "Languages",
-    skillName: "JavaScript, TypeScript",
+    title: "Programming languages",
+    skillName: "Python, Java, C, SQL",
     color: "1",
-    percentage: "80",
+    percentage: "100",
   },
   {
     title: "Frameworks/Libraries",
-    skillName: "Angular, RxJS, NGRX/NGXS",
+    skillName: "QISKIT, Circ, PennyLane, TensorFlow",
     color: "6",
-    percentage: "70",
+    percentage: "90",
   },
   {
-    title: "Design",
-    skillName: "HTML, Bootstrap, Tailwind",
+    title: "Quantum computing",
+    skillName: "Quantum approximate optimization algorithms, Variational quantum algorithms",
     color: "4",
-    percentage: "70",
+    percentage: "90",
   },
   {
-    title: "Version Control",
-    skillName: "GitHub, JIRA, Bitbucket",
+    title: "Mathematical optimization",
+    skillName: "Quadratic unconstrained binary optimization, Mixed integer optimization",
     color: "7",
-    percentage: "70",
+    percentage: "90",
   },
-  {
-    title: "Tools",
-    skillName: "Debugging, Chrome DevTools",
-    color: "3",
-    percentage: "80",
-  },
-//   {
-//     title: "Analytics",
-//     skillName: "CleverTap, Countly",
-//     color: "5",
-//     percentage: "50",
-//   },
-//   {
-//     title: "IDE",
-//     skillName: "VS Code",
-//     color: "6",
-//     percentage: "70",
-//   },
 ];
 
 export const projects = {
   webProjects: [
     {
-      projectName: "Programming Diaries",
+      projectName: "Variational quantum classical QUBO solver",
       image: "images/programmingdiaries.png",
       summary:
-        "Developed a full stack blog application to provide content on techical topics across the internet with admin interface.",
-      preview: "https://programmingdiaries.herokuapp.com/",
-      techStack: ["Django", "SQLite", "Bootstrap", "JavaScript", "Heroku"],
-    },
-    {
-      projectName: "Find Your Bank",
-      image: "images/findyourbank.png",
-      summary:
-        "Developed a React application to render a list of banks fetched from API. Filtered the banks based on queries from localstorage, marked favorites banks.",
-      preview: "https://clever-fermi-0d5d76.netlify.app",
-      techStack: ["Reactjs", "Bootstrap", "JavaScript", "Netlify"],
-    },
-    {
-      projectName: "Web Portfolio",
-      image: "images/portfolio.png",
-      summary:
-        "Web Portfolio to showcase acadmics, skills, projects and contact details in better manner.",
-      preview: "https://github.com/vinaysomawat/Travographer-Portal",
-      techStack: ["HTML", "Bootstrap", "JavaScript"],
-    },
-    {
-      projectName: "Resume Builder",
-      image: "images/resume-builder.png",
-      summary:
-        "Browser based editor to build and download Resumes in a customizable templates.",
-      preview: "https://vinaysomawat.github.io/Resume-Builder",
-      techStack: ["HTML", "Bootstrap", "JavaScript"],
+        "Developed a full stack web application where the user can create multiple optimization problems (Tail assignment, Vehicle routing, etc..) and solve them using a qubit efficient variational quantum algorithm.",
+      preview: "https://github.com/leonidas1312",
+      techStack: ["Streamlit", "Qubit efficient", "Variational quantum classical", "Qiskit", "Tensorflow"],
     },
   ],
   softwareProjects: [
     {
-      projectName: "Pizza Ordering ChatBot",
+      projectName: "Reinforcement learning based enchancement of QUBO solvers",
       image: "images/pizzaorderchatbot.png",
       summary:
-        "ChatBot using Dialogflow, Firebase database which stores the chat data in the realtime database.",
-      preview: "https://github.com/vinaysomawat/Pizza-Ordering-ChatBot",
+        "Classical RL method designed to run on GPUs with the purpose of enchancing classical bitstring solutions using a problem based reward system.",
+      preview: "https://github.com/leonidas1312",
       techStack: ["Dailogflow", "Firebase"],
-    },
-    {
-      projectName: "WhatsApp-Bot",
-      image: "images/whatsappbot.jpg",
-      summary:
-        "Python script which helps to send messages to WhatsApp contacts automatically using selenium and web automation.",
-      preview: "https://github.com/vinaysomawat/WhatsApp-Bot",
-      techStack: ["Selenium", "Chrome Webdriver", "Python"],
-    },
-    {
-      projectName: "Bill Generator",
-      image: "images/billgenerator.png",
-      summary:
-        "GUI to transfer data to excel sheets and generate bills on the local shops.",
-      preview: "https://github.com/vinaysomawat/Bill-Generator",
-      techStack: ["Tkinter", "Openxlpy", "Python"],
-    },
-  ],
-  androidProjects: [
-    {
-      projectName: "NITW-CSE",
-      image: "images/nitwcse.jpg",
-      summary:
-        "The Application display details of Department courses, reference books, research, publication and faculty profile.",
-      preview: "https://github.com/vinaysomawat/NITW-CSE",
-      techStack: ["JAVA", "XML", "Android"],
-    },
-    {
-      projectName: "CareerHigh-App",
-      image: "images/carrerhigh.png",
-      summary:
-        "The Application display the webpages of website careerhigh.in in android devices.",
-      preview: "https://github.com/vinaysomawat/CareerHigh-Android",
-      techStack: ["JAVA", "XML", "Android"],
     },
   ],
   freelanceProjects: [
     {
-      projectName: "SnylloAir.com",
+      projectName: "MoMo trading bot",
       image: "images/snylloair.png",
       summary:
-        "Developed a company website to showcase the purpose, services and products provided by the company to audience.",
-      preview: "https://www.snylloair.com/",
-      techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
-    },
-    {
-      projectName: "Delivery+",
-      image: "images/AM-Logo-.png",
-      summary: "Android Application to display website in android devices.",
-      preview:
-        "https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
-      techStack: ["Android", "JAVA", "Play Store"],
+        "Trained a neural network to predict stock prices with data found online.",
+      preview: "https://github.com/leonidas1312/MoMo-trading-bot",
+      techStack: ["Streamlit", "PyTorch", "yFinance"],
     },
   ],
 };
