59c6c38d56260ef7fdc99fd1fdab0cbfcea76917
leonidas <49210924+leonidas1312@users.noreply.github.com>
2024-10-08 12:48:44 +0300

    Update urls.js

--- user-data/urls.js
+++ user-data/urls.js
@@ -1,5 +1,5 @@
-const githubUsername = "vinaysomawat";
-const mediumUsername = "vinaysomawat";
+const githubUsername = "Ioannis D. Leonidas";
+const mediumUsername = "Ioannis D. Leonidas";
 
 const createMediumURL = (username) => `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@${username}`;
 const createGitConnectedURL = (username) => `https://gitconnected.com/v1/portfolio/${username}`;
