f2828d467067db621c737dd3c976997503372892
leonidas <49210924+leonidas1312@users.noreply.github.com>
2024-10-10 17:17:47 +0300

    Update profile-card.js

--- js/profile-card.js
+++ js/profile-card.js
@@ -2,14 +2,14 @@ window.addEventListener("DOMContentLoaded", async function () {
     // List of articles
     const articles = [
         {
-            title: "Vehicle Routing with Efficient Qubits",
+            title: "Vehicles routed with efficient use of qubits @ Singapore Quantum Technologies website",
             link: "https://www.quantumlah.org/about/highlight/2024-06-vehicle-routing-efficient-qubits",
-            description: "This article discusses quantum techniques applied to vehicle routing optimization."
+            description: "A highlight of the importance of our research work using real data from ExxonMobil."
         },
         {
-            title: "Postgraduate Thesis Publication",
+            title: "Postgraduate Thesis Publication @ Technical University of Crete website",
             link: "https://www.ece.tuc.gr/en/news/item/publication-of-the-postgraduate-thesis-of-a-graduate-student-of-the-ece-school-in-a-high-level-international-research-journal",
-            description: "A highlight of a thesis published in a top international research journal."
+            description: "A highlight of my thesis published in a top international research journal."
         }
     ];
 
@@ -18,7 +18,7 @@ window.addEventListener("DOMContentLoaded", async function () {
         const article = articles[index];
 
         el.innerHTML = `
-            <div style="font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif; border-radius: 6px; line-height: 1.5; padding: 16px; font-size: 14px; color: #24292e; background-color: #f9bf3f;">
+            <div style="font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif; border-radius: 6px; line-height: 1.5; padding: 16px; font-size: 14px; color: #000000; background-color: #f9bf3f;">
                 <h3><a href="${article.link}" target="_blank">${article.title}</a></h3>
                 <p>${article.description}</p>
             </div>
